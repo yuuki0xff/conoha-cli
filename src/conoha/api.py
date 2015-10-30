173ea77734025e054949a039152838b3566a2de4
yuuki <yuuki0xff@gmail.com>
2015-10-31 01:28:35 +0900

    トークンが取得できるようになった

--- src/conoha/api.py
+++ src/conoha/api.py
@@ -1,26 +1,42 @@
 
 # -*- coding: utf8 -*-
-import urllib.request
+from urllib.request import Request, urlopen
 import json
 __ALL__ = 'API Tenant'.split()
 
 class API:
 	baseURI = 'https://identity.tyo1.conoha.io'
 
-	def _GET(self, path, baseURI=self.baseURI):
-		res = urllib.request.urlopen(baseURI+path).read()
-		return json.loads(res)
+	def _getHeaders(self):
+		headers={
+				'Accept': 'application/json',
+				}
+		return headers
 
-	def _POST(self, path, data, baseURI=self.baseURI):
-		data = json.dujps(data)
-		res = urllib.request.urlopen(baseURI+path, data=data).read()
-		return json.loads(res)
+	def _GET(self, path, baseURI=None):
+		req = Request(
+				url=(baseURI or self.baseURI),
+				headers=self._getHeaders(),
+				)
+		with urlopen(req) as res:
+			return json.loads(res.read())
+
+	def _POST(self, path, data, baseURI=None):
+		data = bytes(json.dumps(data), 'utf8')
+		req = Request(
+				url=(baseURI or self.baseURI)+path,
+				data=data,
+				method='POST',
+				headers=self._getHeaders(),
+				)
+		with urlopen(req) as res:
+			return json.loads(str(res.read(), 'utf8'))
 
 class Identity(API):
 	baseURI = 'https://identity.tyo1.conoha.io'
 	token = None
 
-	def getToken(self, userName, password, tenantId=None):
+	def __init__(self, userName, password, tenantId=None):
 		path = '/v2.0/tokens'
 		data = { 'auth':{
 			'passwordCredentials':{
@@ -28,29 +44,8 @@ class Identity(API):
 				'password': password,
 				},
 			}}
+		if tenantId:
+			data['auth']['passwordCredentials']['tenantId'] = tenantId
 		res = self._POST(path, data)
-		self.token = Token(res['access']['token'])
-
-class Token:
-	rawData = None
-	def __init__(self, jsonToken):
-		self.rawData = jsonToken
-
-	def getTenant(self, typeName):
-		for tenant in self.rawData['tenant']:
-			if tenant['type'] != typeName:
-				continue
-			return Tenant(tenant['type'])
-
-class Tenant:
-	rawData = None
-
-	def __init__(self, jsonTenant):
-		self.rawData = jsonTenant
-
-	def getEndpointURI(self, region):
-		for endpoint in self.rawData['endpoints']:
-			if endpoint['region'] != region:
-				continue
-			return endpoint['publicURL']
+		self.token = res['access']['token']
 
