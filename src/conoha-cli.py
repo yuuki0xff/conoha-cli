9f5d0eb084144cf9ce1c0d79c042c58f0c4b2b83
yuuki <yuuki0xff@gmail.com>
2015-11-11 19:22:16 +0900

    refactor

--- src/conoha-cli.py
+++ src/conoha-cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 from conoha.config import Config
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser
 import sys
 
 def main():
