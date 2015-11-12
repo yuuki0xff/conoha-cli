aed50a8093819fbae904c93e2979c3130eb4aa58
yuuki <yuuki0xff@gmail.com>
2015-11-12 19:29:05 +0900

    conoha-cli.py: サブコマンドのパーサ設定を、サブコマンドのクラスに追い出す

--- src/conoha-cli.py
+++ src/conoha-cli.py
@@ -21,45 +21,49 @@ def getArgumentParser():
 
 	parser_compute = subparser.add_parser('compute')
 	subparser_compute = parser_compute.add_subparsers()
-
-	parser_compute_plans = subparser_compute.add_parser('plans')
-
-	parser_compute_list = subparser_compute.add_parser('list')
-	parser_compute_list.add_argument('--verbose', action='store_true')
-
-	parser_compute_add = subparser_compute.add_parser('add')
-	parser_compute_add.add_argument('--quiet', action='store_true')
-	parser_compute_add.add_argument('--name', type=str)
-	parser_compute_add.add_argument('--passwd', type=str)
-	parser_compute_add.add_argument('--key', type=str)
-
-	parser_compute_status = subparser_compute.add_parser('status')
-	parser_compute_status.add_argument('--name', type=str)
-	parser_compute_status.add_argument('--id', type=str)
-
-	parser_compute_start = subparser_compute.add_parser('start')
-	parser_compute_start.add_argument('--name', type=str)
-	parser_compute_start.add_argument('--id', type=str)
-
-	parser_compute_stop = subparser_compute.add_parser('stop')
-	parser_compute_stop.add_argument('--name', type=str)
-	parser_compute_stop.add_argument('--id', type=str)
-	parser_compute_stop.add_argument('--force', action='store_true')
-
-	parser_compute_reboot = subparser_compute.add_parser('reboot')
-	parser_compute_reboot.add_argument('--name', type=str)
-	parser_compute_reboot.add_argument('--id', type=str)
-
-	parser_compute_delete = subparser_compute.add_parser('delete')
-	parser_compute_delete.add_argument('--name', type=str)
-	parser_compute_delete.add_argument('--id', type=str)
-
-	parser_compute_resize = subparser_compute.add_parser('resize')
-	parser_compute_resize.add_argument('--name', type=str)
-	parser_compute_resize.add_argument('--id', type=str)
-	parser_compute_resize.add_argument('--plan', type=str)
+	ComputeCommand.configureParser(subparser_compute)
 
 	return parser
+
+class ComputeCommand():
+	@classmethod
+	def configureParser(cls, subparser):
+		plans = subparser.add_parser('plans')
+
+		list_ = subparser.add_parser('list')
+		list_.add_argument('--verbose', action='store_true')
+
+		add = subparser.add_parser('add')
+		add.add_argument('--quiet', action='store_true')
+		add.add_argument('--name', type=str)
+		add.add_argument('--passwd', type=str)
+		add.add_argument('--key', type=str)
+
+		status = subparser.add_parser('status')
+		status.add_argument('--name', type=str)
+		status.add_argument('--id', type=str)
+
+		start = subparser.add_parser('start')
+		start.add_argument('--name', type=str)
+		start.add_argument('--id', type=str)
+
+		stop = subparser.add_parser('stop')
+		stop.add_argument('--name', type=str)
+		stop.add_argument('--id', type=str)
+		stop.add_argument('--force', action='store_true')
+
+		reboot = subparser.add_parser('reboot')
+		reboot.add_argument('--name', type=str)
+		reboot.add_argument('--id', type=str)
+
+		delete = subparser.add_parser('delete')
+		delete.add_argument('--name', type=str)
+		delete.add_argument('--id', type=str)
+
+		resize = subparser.add_parser('resize')
+		resize.add_argument('--name', type=str)
+		resize.add_argument('--id', type=str)
+		resize.add_argument('--plan', type=str)
 
 if __name__ == '__main__':
 	exit(main())
