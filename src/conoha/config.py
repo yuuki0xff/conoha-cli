76f6b0c92777ec94e03e7427a4fc76894371c576
yuuki <yuuki0xff@gmail.com>
2015-11-12 19:00:24 +0900

    config.py: デフォルト値を用意 & 環境変数からの設定に対応

--- src/conoha/config.py
+++ src/conoha/config.py
@@ -6,8 +6,26 @@ from configparser import SafeConfigParser
 __ALL__ = 'Config'.split()
 
 class Config(SafeConfigParser):
+	_defaultValue = {
+			'api': {
+				'user': '',
+				'passwd': '',
+				'tenant': '',
+
+				'timeout': 30,
+				'retry': 0,
+				},
+			'compute': {
+				'soft_limit': -1,
+				'hard_limit': -1,
+				'allow_plans': '',
+				},
+			}
+
 	def __init__(self):
 		super().__init__()
+		self._setDefaultValue()
+		self._readEnv()
 		os.environ.setdefault('XDG_CONFIG_HOME', '~/.config')
 		self.read(self._pathExpand_([
 			'$XDG_CONFIG_HOME/conoha/config',
@@ -18,4 +36,23 @@ class Config(SafeConfigParser):
 		for i, item in enumerate(pathList):
 			pathList[i] = os.path.expanduser(os.path.expandvars(item))
 		return pathList
+
+	def _setDefaultValue(self):
+		self.read_dict(self._defaultValue)
+
+	def _readEnv(self):
+		for key in os.environ:
+			if not key.startswith('CONOHA_') or len(key.split('_')) != 3:
+				continue
+
+			section = key.split('_')[1].upper()
+			if section.lower() not in self._defaultValue:
+				continue
+
+			parameter = key.split('_')[2].upper()
+			if parameter.lower() not in self._defaultValue[section]:
+				continue
+
+			if section and parameter:
+				self[section][parameter] = os.environ[key]
 
