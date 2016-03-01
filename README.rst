8c6f4130475cb17ef5484cea151082e47d34f01a
yuuki <yuuki0xff@gmail.com>
2016-03-01 11:48:16 +0900

    update setup.py

---
+++ README.rst
@@ -0,0 +1,37 @@
+conoha-cli
+==========
+**conoha-cli** は `ConoHa <https://www.conoha.jp/>`_ API のコマンドラインインターフェースとPython3用ライブラリです。
+
+インストール
+============
+`conoha-cli` はこのコマンドを実行するとインストールでいます::
+
+    $ pip3 install conoha-cli
+
+次に、`~/.config/conoha/config` へこのようなファイルを作成してください。
+user, passwd, tenantは、`Conohaコントロールパネルから作成したAPIユーザ <https://www.conoha.jp/guide/g-46.html>`_ の値を入力してください。::
+
+    [api]
+    user = xxxxx
+    passwd = xxxxx
+    tenant = xxxxx
+
+使い方
+======
+VPSの一覧を確認する::
+
+    $ conoha-cli compute list-vms
+
+マシンリーダブルな形式でも出力できます::
+
+    $ conoha-cli --format plain --header no compute list-vms
+
+VPSを起動・終了・削除する::
+
+    $ conoha-cli compute start-vm --name $VM_NAME
+    $ conoha-cli compute stop-vm  --name $VM_NAME
+    $ conoha-cli compute delete-vm --name $VM_NAME
+
+ライセンス
+==========
+MIT
--- setup.py
+++ setup.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import textwrap
 
 setup(name='conoha-cli',
       version='0.0.4',
       description='conoha-cli is a command and Python3 library for ConoHa API.',
+      long_description=open('README.rst', 'r', encoding='utf8').read(),
+      keywards='conoha cli api library cloud',
       url='https://github.com/yuuki0xff/conoha-cli',
       author='yuuki0xff',
       author_email='yuuki0xff@gmail.com',
       license='MIT',
       packages=['conoha'],
       zip_safe=False,
+      classifiers=textwrap.dedent("""
+        Development Status :: 5 - Production/Stable
+        Environment :: Console
+        Intended Audience :: Developers
+        Intended Audience :: System Administrators
+        License :: OSI Approved :: MIT License
+        Natural Language :: Japanese
+        Operating System :: POSIX :: Linux
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3 :: Only
+        Programming Language :: Python :: Implementation :: CPython
+        Topic :: Utilities
+        """).strip().splitlines(),
       install_requires=['tabulate'],
       entry_points={
-		  'console_scripts': [
-			  'conoha-cli = conoha.cli:main',
-		  	  ],
-      	  },
+          'console_scripts': [
+              'conoha-cli = conoha.cli:main',
+              ],
+          },
       )
+
+# vim: expandtab
