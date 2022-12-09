e2701e0c53915e949366282916c5d4b894a612ff
yuuki <yuuki0xff@gmail.com>
2022-12-09 22:20:42 +0900

    v0.3.0

--- conoha/__init__.py
+++ conoha/__init__.py
@@ -6,5 +6,5 @@ Python3 library for ConoHa(https://www.conoha.jp/) API.
 from . import api, config, compute, network, block
 
 __author__ = 'yuuki0xff (https://github.com/yuuki0xff)'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
